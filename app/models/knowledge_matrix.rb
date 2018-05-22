f83a650a8458b579ba4564f6f6ce987de5171c91
Hebert Douglas <hebertdougl@gmail.com>
2018-05-22 19:08:05 -0300

    Create tests to KnowledgeMatrix.

--- app/models/knowledge_matrix.rb
+++ app/models/knowledge_matrix.rb
@@ -1,6 +1,6 @@
 class KnowledgeMatrix < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
-  validates :know_description, presence: true
   validates :for_description, presence: true
+  validates :know_description, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :sequence, presence: true, uniqueness: true
 end
