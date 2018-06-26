960fa36f1785af8aa9dbb85869f1fa8fb2d478f2
Hebert Douglas <hebertdougl@gmail.com>
2018-06-26 15:02:24 -0300

    - Add color to Curricular Components - Add tests - Fix Sustainable development Goals colors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_26_124021) do
+ActiveRecord::Schema.define(version: 2018_06_26_170737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 2018_06_26_124021) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug", null: false
+    t.string "color"
     t.index ["slug"], name: "index_curricular_components_on_slug", unique: true
   end
 
