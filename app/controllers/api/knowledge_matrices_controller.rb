1c44b709551459bff024073bc207415db2884f45
Hebert Douglas <hebertdougl@gmail.com>
2018-07-02 19:13:16 -0300

    Modify /api/saberes /api/ods order - decreasing the number of active storage requests

--- app/controllers/api/knowledge_matrices_controller.rb
+++ app/controllers/api/knowledge_matrices_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class KnowledgeMatricesController < ApiController
     def index
-      @knowledge_matrices = KnowledgeMatrix.all
+      @knowledge_matrices = KnowledgeMatrix.all.order(:sequence)
       raise ActiveRecord::RecordNotFound unless @knowledge_matrices.present?
 
       render :index
