9c17ad41192f804187159daa6f7ec8c642c1524d
Arthur Lima <malungo@gmail.com>
2018-07-17 16:06:05 -0300

    rollback puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,58 +1,17 @@
-# Change to match your CPU core count
-workers 2
 
-# Min and Max threads per worker
-threads 1, 6
-
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-
-# Default to production
-rails_env = ENV['RAILS_ENV'] || "production"
-environment rails_env
-
-# Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
-
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-# Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
-
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+app_path = ENV.fetch("APP_PATH") { "/app" }
+
+workers 2
 threads threads_count, threads_count
-
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PUMA_PORT") { 8666 }
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
-
-# Allow puma to be restarted by `rails restart` command.
+port 8666
+environment rails_env
+if rails_env != "development"
+  stdout_redirect "#{app_path}/log/#{rails_env}.log",
+                  "#{app_path}/log/#{rails_env}.error.log",
+                  true
+end
+state_path "#{app_path}/shared/pids/puma.state"
+pidfile "#{app_path}/shared/pids/puma.pid"
 plugin :tmp_restart
