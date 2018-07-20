5a926390c21092ce385a125e52093467a7f14811
galeto <vsmont.monteiro@gmail.com>
2018-07-20 19:43:39 -0300

    name

--- app/models/destroy_validator.rb
+++ app/models/destroy_validator.rb
@@ -1,15 +1,15 @@
 module DestroyValidator
   def check_associations(associations)
     associations.each do |association|
-      a = send(association)
-      if a.present?
+      _association = send(association)
+      if _association.present?
         klass = nil
         message = 'activerecord.errors.messages.restrict_dependent_destroy.'
-        if a.try(:last)
-          klass = a.try(:last).try(:class)
+        if _association.try(:last)
+          klass = _association.try(:last).try(:class)
           message += 'has_many'
         else
-          klass = a.class
+          klass = _association.class
           message += 'has_one'
         end
         errors.add(association, I18n.t(message, record: klass.model_name.human))
