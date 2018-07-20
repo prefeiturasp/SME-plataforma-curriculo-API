344f30619b7a8242c07ad2c38e48b8889903d724
galeto <vsmont.monteiro@gmail.com>
2018-07-20 19:58:47 -0300

    refactor

--- app/models/destroy_validator.rb
+++ app/models/destroy_validator.rb
@@ -1,20 +1,19 @@
 module DestroyValidator
   def check_associations(associations)
     associations.each do |association|
-      _association = send(association)
-      if _association.present?
-        klass = nil
-        message = 'activerecord.errors.messages.restrict_dependent_destroy.'
-        if _association.try(:last)
-          klass = _association.try(:last).try(:class)
-          message += 'has_many'
-        else
-          klass = _association.class
-          message += 'has_one'
-        end
-        errors.add(association, I18n.t(message, record: klass.model_name.human))
-        throw(:abort)
+      instance = send(association)
+      next unless instance.present?
+      klass = nil
+      message = 'activerecord.errors.messages.restrict_dependent_destroy.'
+      if instance.try(:last)
+        klass = instance.try(:last).try(:class)
+        message += 'has_many'
+      else
+        klass = instance.class
+        message += 'has_one'
       end
+      errors.add(association, I18n.t(message, record: klass.model_name.human))
+      throw(:abort)
     end
   end
 end
