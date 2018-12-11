61bead8b6bbfd4f921d5ab298f56981975322999
Hebert Douglas <hebertdougl@gmail.com>
2018-12-11 19:19:14 -0200

    - Refactor fields divs - Add css style - Add activity content block structure - Fix remove button

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -5,14 +5,11 @@ module Admin
     def link_to_add_fields(name, f, association, content_type)
       new_object = f.object.class.reflect_on_association(association).klass.new
       partials_path = ContentBlock.partials_path
+      content_block = ContentBlock.find_by(content_type: content_type)
       fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-        render(partials_path + content_type.to_s.singularize + "_fields", :f => builder)
+        render(partials_path + content_type.to_s.singularize + "_fields", :f => builder, content_block: content_block)
       end
       fields = "<fieldset class='inputs has_many_fields new_content #{content_type}'><ol>#{fields.to_s}</ol></fieldset>"
-
-      # Rails.logger.debug(">"*80)
-      # Rails.logger.debug(fields.inspect)
-      # Rails.logger.debug("<"*80)
       
       father = 'asdf asdf'
       link_to "#{ name }", '#', onclick: h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields.to_s)}\", \"#{father}\"); return false;"), data: { id: 'id' }, class: "button #{content_type}"
