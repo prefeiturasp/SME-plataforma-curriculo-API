486761907818f01a9a11c9466d961a7940c1b746
Hebert Douglas <hebertdougl@gmail.com>
2018-12-12 12:26:53 -0200

    Add migrate to assign default schema to content blocks.

---
+++ db/migrate/20181212142404_assign_default_schema_to_content_block.rb
@@ -0,0 +1,119 @@
+class AssignDefaultSchemaToContentBlock < ActiveRecord::Migration[5.2]
+  def up
+    [
+      {
+        "content_type": "to_teacher",
+        "schema": {
+          "required": ["body"],
+          "properties": {
+            "body": {
+              "type": "string"
+            }
+          }
+        }
+      },
+      {
+        "content_type": "to_student",
+        "schema": {
+          "required": ["body"],
+          "properties": {
+            "body": {
+              "type": "string"
+            }
+          }
+        }
+      },
+      {
+        "content_type": "question",
+        "schema": {
+          "required": ["title", "number", "body"],
+          "properties": {
+            "title": {
+              "type": "string"
+            },
+            "number": {
+              "type": "number"
+            },
+            "body": {
+              "type": "json"
+            }
+          }
+        }
+      },
+      {
+        "content_type": "predefined_exercise",
+        "schema": {
+          "required": ["exercise_type", "body"],
+          "properties": {
+            "exercise_type": {
+              "type": "string",
+              "enum": [
+                "Atividade prática",
+                "Calcule",
+                "Ouça o professor",
+                "Fique atento",
+                "Laboratório de informática",
+                "Vamos pesquisar",
+                "Música",
+                "Para saber mais",
+                "Recitação numérica",
+                "Roda de conversa",
+                "Sala de leitura",
+                "Tome nota"
+              ]
+            },
+            "body": {
+              "type": "string"
+            },
+            "icon_url": {
+              "type": "string"
+            }
+          }
+        }
+      },
+      {
+        "content_type": "long_text",
+        "schema": {
+          "required": ["title", "body"],
+          "properties": {
+            "title": {
+              "type": "string"
+            },
+            "body": {
+              "type": "json"
+            }
+          }
+        }
+      },
+      {
+        "content_type": "gallery",
+        "schema": {
+          "properties": {
+            "subtitle": {
+              "type": "string"
+            },
+            "file": {
+              "type": "file"
+            }
+          }
+        }
+      },
+      {
+        "content_type": "free_text",
+        "schema": {
+          "required": ["body"],
+          "properties": {
+            "body": {
+              "type": "json"
+            }
+          }
+        }
+      }
+    ].each do |attributes|
+      cb = ContentBlock.find_or_create_by(content_type: attributes[:content_type])
+      schema = attributes[:schema]
+      cb.json_schema = schema.to_json
+      cb.save
+    end
+  end
+end
