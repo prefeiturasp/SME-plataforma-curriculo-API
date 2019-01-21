24b848f71740649cbe313a18580e4d532da1254b
Hebert Douglas <hebertdougl@gmail.com>
2019-01-21 17:39:17 -0200

    Remove deprecation warning from static attributes on factories.

--- spec/factories/activity_sequence_performeds.rb
+++ spec/factories/activity_sequence_performeds.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :activity_sequence_performed do
     association :activity_sequence, factory: :activity_sequence
     association :teacher, factory: :teacher
-    evaluated false
+    evaluated { false }
   end
 end
