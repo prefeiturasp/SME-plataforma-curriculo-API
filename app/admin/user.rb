856ac15f0a3986747ad195dd633c7b3537846db5
Hebert Douglas <hebertdougl@gmail.com>
2019-02-05 19:30:23 -0200

    Change scoped collection on admin users area.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,6 +3,13 @@ ActiveAdmin.register User do
 
   config.filters = false
 
+  controller do
+    def scoped_collection
+      User.where(username: nil)
+    end
+  end
+
+
   index do
     selectable_column
     id_column
@@ -14,6 +21,7 @@ ActiveAdmin.register User do
   end
 
   form do |f|
+    f.semantic_errors *f.object.errors.keys
     f.inputs do
       f.input :email
       f.input :password
