cabf98545cda8b370f2138e03f2d8f2e71a810d7
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2020-04-24 17:42:20 -0300

    Feature: add validations presence to field name in Segments and Stages tests.

--- spec/models/stage_spec.rb
+++ spec/models/stage_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Stage, type: :model do
   it { is_expected.to have_db_column(:name).of_type(:string) }
+  it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to belong_to(:segment) }
 end
