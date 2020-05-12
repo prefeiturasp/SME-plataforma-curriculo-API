14f833ce3121b08918b73d630597b3d0799edba3
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2020-05-12 08:47:27 -0300

    Feature: add association.

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,6 +1,8 @@
 class Segment < ApplicationRecord
   has_many :stages
   has_many :answer_books
+  has_many :activity_sequence
+  has_many :learning_objective
 
   validates :name, presence: true
 end
--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,6 +1,16 @@
 class Stage < ApplicationRecord
   belongs_to :segment
   has_many :answer_book
-  
+  has_many :activity_sequence
+  has_many :learning_objective
+
   validates :name, presence: true
+
+  class << self
+
+    def all_or_with_segment(segment_id)
+      return [] unless segment_id
+      Stage.where(segment_id: segment_id)
+    end
+  end
 end
