3e5ef2d91ca8bd4ad82216adba205181d05cf32c
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2020-05-30 07:50:00 -0300

    Feature: rename file.

--- db/migrate/20200530070121_change_year_from_answer_books.rb
+++ db/migrate/20200530070121_change_year_from_answer_books.rb
@@ -1,4 +1,4 @@
-class RemoveYearFromAnswerBooks < ActiveRecord::Migration[5.2]
+class ChangeYearFromAnswerBooks < ActiveRecord::Migration[5.2]
   def change
     remove_column :answer_books, :year
     add_reference :answer_books, :year, index: true
