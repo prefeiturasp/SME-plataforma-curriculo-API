b103de135dc226701a33311eb89e674a091d932d
RodolphoCLobo <rodolpho1704@gmail.com>
2020-06-26 14:59:28 -0300

    Feature: Create Survey Forms and Survey Form Questions for admin.

--- app/assets/javascripts/activities/form-sortable-list.js
+++ app/assets/javascripts/activities/form-sortable-list.js
@@ -1,8 +1,20 @@
 function updateContentBlockSequenceOnDatabase() {
-  var activity_id = $('#activity_id, #challenge_id').val();
-  var post_url = `/admin/activities/${activity_id}/activity_content_blocks/set_order`;
   var order = $('#sortable-list').sortable('toArray');
-  var post_data = { activity_content_block_ids: order };
+  if ($('#activity_id').val()) {
+    var activity_id = $('#activity_id').val();
+    var post_url = `/admin/activities/${activity_id}/activity_content_blocks/set_order`;
+    var post_data = { activity_content_block_ids: order };
+  }
+  if ($('#challenge_id').val()) {
+    var challenge_id = $('#challenge_id').val();
+    var post_url = `/admin/challenges/${challenge_id}/challenge_content_blocks/set_order`;
+    var post_data = { challenge_content_block_ids: order };
+  }
+  if ($('#survey_form_id').val()) {
+    var survey_form_id = $('#survey_form_id').val();
+    var post_url = `/admin/survey_forms/${survey_form_id}/survey_form_content_blocks/set_order`;
+    var post_data = { survey_form_content_block_ids: order };
+  }
   $.post(post_url, post_data, function(){}, 'json');
 }
 
@@ -40,7 +52,7 @@ function setSequenceInContentBlocks(){
 function setSortableList(){
   $('#sortable-list').sortable({
     update: function() {
-      $('form#edit_activity, form#edit_challenge').length > 0 ? updateContentBlockSequenceOnDatabase() : null;
+      $('form#edit_activity, form#edit_challenge, form#edit_survey_form').length > 0 ? updateContentBlockSequenceOnDatabase() : null;
       setSequenceInContentBlocks();
       moveContentBlocksAfterSortable();
     }
