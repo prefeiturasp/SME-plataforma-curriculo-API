ebaa6c0b882c91512beacbba4649ed3aa8c98c48
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2020-11-30 13:11:43 -0300

    Fix: end time to final of day.

--- app/assets/javascripts/public_consultation.js
+++ app/assets/javascripts/public_consultation.js
@@ -5,4 +5,10 @@ $(document).ready(function(){
   $('#public_consultation_documents').change(function(){
     $('.public_consultation_file').hide();
   });
+  $('#public_consultation_initial_date').change(function(){
+    $('#public_consultation_initial_date')[0].value = `${$('#public_consultation_initial_date')[0].value} 00:00:00`
+  });
+  $('#public_consultation_final_date').change(function(){
+    $('#public_consultation_final_date')[0].value = `${$('#public_consultation_final_date')[0].value} 23:59:59`
+  });
 });
