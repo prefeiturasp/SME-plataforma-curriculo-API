90fd2e676d9213de545287153815b8689f001c7b
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2023-10-05 15:23:27 -0300

    correção testes

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,7 +1,7 @@
 require 'webmock/rspec'
 WebMock.enable!
 
-allowed_sites = ['http://elasticsearch:9200', 'http://locahost']
+allowed_sites = ['http://elasticsearch:9200', 'http://localhost', 'http://localhost:9200']
 WebMock.disable_net_connect!(allow: allowed_sites)
 
 RSpec.configure do |config|
