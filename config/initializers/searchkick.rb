c595e5f4a8b60419ef08166f559c4c0229256496
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2023-10-05 14:24:05 -0300

    ajustes testes

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,5 +1,5 @@
 Searchkick.client = Elasticsearch::Client.new(
-  hosts: ["elasticsearch:9200"],
+  hosts: ["localhost:9200"],
   retry_on_failure: true,
   transport_options: { request: {timeout: 250} }
 )
--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,7 +1,7 @@
 require 'webmock/rspec'
 WebMock.enable!
 
-allowed_sites = ['http://elasticsearch:9200', 'http://locahost']
+allowed_sites = ['http://elasticsearch:9200', 'http://locahost','http://locahost:9200']
 WebMock.disable_net_connect!(allow: allowed_sites)
 
 RSpec.configure do |config|
